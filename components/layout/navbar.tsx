a5e84420aeb93dae96e6ab22b6b34905d203cf81
Ashir Arif <techashir167@gmail.com>
2026-03-12 15:20:41 +0500

    new changes

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="container absolute top-0 left-0 right-0 w-full z-50 flex items-center justify-center py-6">
-        <Image src="/images/svg/logo.svg" alt="Logo" width={200} height={100} />
+        <Image src="/images/svg/logo.svg" alt="Logo" width={150} height={90} />
       </nav>
     </>
   );
